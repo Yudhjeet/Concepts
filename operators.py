448e40dbb0c6ce7cb871b217e7e4d4e59d4d182d
Yudhjeet <Yudhjeetcodes@gmail.com>
2023-11-09 10:47:22 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+#Every Arithmetic Operator and their output
+
+a= 10
+b= 5
+
+sum= a+b
+sub= a-b
+multiply= a*b
+divide= a/b
+modulus= a%b
+exponent= a**b
+floor= a//b
+
+print("Sum:", sum, "Subtract:", sub, "Multiply:", multiply, "Divide:", divide, "Modulus:", modulus, "Exponent:", exponent, "Floor:", floor)
